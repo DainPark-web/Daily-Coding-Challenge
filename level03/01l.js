7dc8c8aad10e91d7b1776f326b63a0b1c845ede4
dain <57316771+DainPark-web@users.noreply.github.com>
2022-01-29 16:46:47 +0100

    updda

--- level03/01l.js
+++ level03/01l.js
@@ -18,9 +18,10 @@ var uniqueInOrder=function(iterable){
             return a;
         }
     })
+    console.log(result);
     return result;
   }
-
+uniqueInOrder([1,2,2,3,3]);
 
 //result
 // uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
