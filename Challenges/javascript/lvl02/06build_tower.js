e070368c0cbad3b5c851a9b079e9bb28eba411f6
dain <57316771+DainPark-web@users.noreply.github.com>
2022-01-30 14:01:56 +0100

    update

--- Challenges/javascript/lvl02/06build_tower.js
+++ Challenges/javascript/lvl02/06build_tower.js
@@ -1,5 +1,4 @@
 function towerBuilder(nFloors) {
-    // build here
     let tower = [];
     for(let i = 0; i < nFloors; i++){
         tower.push(" ".repeat(nFloors - i - 1) + "*".repeat((i * 2) + 1) + " ".repeat(nFloors - i - 1))
