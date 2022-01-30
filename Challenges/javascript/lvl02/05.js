cb56723ed2b97127cb48936d3a479d5e254bde71
dain <57316771+DainPark-web@users.noreply.github.com>
2022-01-30 10:23:45 +0100

    update

--- Challenges/javascript/lvl02/05.js
+++ Challenges/javascript/lvl02/05.js
@@ -1,17 +1,20 @@
 function findOutlier(integers){
-   const evenA = integers.filter((item) => item % 2 === 0 );
-   const oddA = integers.filter((item) => item % 2 === 1 );
+   const evenA = integers.filter((item) => Math.abs(item % 2) === 0 );
+   const oddA = integers.filter((item) => Math.abs(item % 2) === 1 );
    
-   if(evenA.length === 1){
+   if(evenA.length < oddA.length){
        return Number(evenA.join(""))
    }
-   if(oddA.length === 1){
+   if(oddA.length < evenA.length){
        return Number(oddA.join(""));
+   }
+   if(evenA.length === oddA.length){
+       return null;
    }
   
   }
 
-
+// console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]));
 //   [2, 4, 0, 100, 4, 11, 2602, 36]
 //   Should return: 11 (the only odd number)
   
