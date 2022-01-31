a5d2f33474c988deed95ff9e0d5d9834fb1e9dd4
dain <57316771+DainPark-web@users.noreply.github.com>
2022-01-31 13:17:45 +0100

    update

--- Challenges/javascript/lvl03/01Directions.js
+++ Challenges/javascript/lvl03/01Directions.js
@@ -6,47 +6,63 @@ function dirReduc(arr){
     let result = []
 
     
+    const northDir = dir.filter((item, index) => item === "NORTH");
+    const southDir = dir.filter((item, index) => item === "SOUTH");
+    const eastDir = dir.filter((item, index) => item === "EAST");
+    const westDir = dir.filter((item, index) => item === "WEST");
 
-    dir.forEach((item, index) => {
+    
+
+    if(northDir.length === 1 && southDir.length ===1 && eastDir.length ===1 && westDir.length ===1){
+      result.push(northDir[0])
+      result.push(westDir[0])
+      result.push(southDir[0])
+      result.push(eastDir[0])
+    }else{
+
+      dir.forEach((item, index) => {
      
-      if(item === "NORTH"){
-        const found = dir.indexOf("SOUTH");
-        if(found !== -1){
-          dir.splice(found, 1, null);
-          dir.splice(index, 1, null);
+        if(item === "NORTH"){
+          const found = dir.indexOf("SOUTH");
+          if(found !== -1){
+            dir.splice(found, 1, null);
+            dir.splice(index, 1, null);
+          }
         }
-      }
-
-      if(item === "SOUTH"){
-        const found = dir.indexOf("NORTH");
-        if(found !== -1){
-          dir.splice(found, 1, null);
-          dir.splice(index, 1, null);
+  
+        if(item === "SOUTH"){
+          const found = dir.indexOf("NORTH");
+          if(found !== -1){
+            dir.splice(found, 1, null);
+            dir.splice(index, 1, null);
+          }
         }
-      }
-      if(item === "EAST"){
-        const found = dir.indexOf("WEST");
-        if(found !== -1){
-          dir.splice(found, 1, null);
-          dir.splice(index, 1, null);
+        if(item === "EAST"){
+          const found = dir.indexOf("WEST");
+          if(found !== -1){
+            dir.splice(found, 1, null);
+            dir.splice(index, 1, null);
+          }
         }
-      }
-      if(item === "WEST"){
-        const found = dir.indexOf("EAST");
-        if(found !== -1){
-          dir.splice(found, 1, null);
-          dir.splice(index, 1, null);
+        if(item === "WEST"){
+          const found = dir.indexOf("EAST");
+          if(found !== -1){
+            dir.splice(found, 1, null);
+            dir.splice(index, 1, null);
+          }
         }
-      }
-
-
-      result = dir.filter((item) => {
-        if(item !== null){
-          return item;
-        }
+  
+  
+        result = dir.filter((item) => {
+          if(item !== null){
+            return item;
+          }
+        })
+  
       })
+    }
 
-    })
+    
     
    return result;
   }
