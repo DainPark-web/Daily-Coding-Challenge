d0fadbe4827e92de8acc838fb5345c66b53a9adf
dain <57316771+DainPark-web@users.noreply.github.com>
2022-01-31 08:40:25 +0100

    update

---
+++ Challenges/javascript/lvl03/01Directions.js
@@ -0,0 +1,43 @@
+function dirReduc(arr){
+    const dir = arr;
+
+    
+
+
+    // dir.filter((item, index) => {
+    //     if(item === "NORTH" ){
+
+    //     }
+    // })
+
+    // for(let i = 0; i<dir.length; i++){
+    //     let found = "";
+    //     let fi = dir[i];
+    //     if(fi === "NORTH"){
+    //        found = dir.findIndex(e => e === "SOUTH");
+    //     }else if(fi === "SOUTH"){
+    //         found = dir.findIndex(e => e === "NORTH")
+    //     }else if(fi === "WEST"){
+    //         found = dir.findIndex(e => e === "EAST")
+    //     }else if(fi === "EAST"){
+    //         found = dir.findIndex(e => e === "WEST")
+    //         console.log(found);
+    //     }else{
+    //         // console.log("error");
+    //     }
+    //     console.log(fi, found, i, dir.length)
+        
+    //         dir.splice(i , 0);
+    //         dir.splice(found, 0);
+          
+        
+
+    // }
+    
+    console.log(dir);
+    
+  }
+
+
+// dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]);
+// ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]), ["WEST"]
