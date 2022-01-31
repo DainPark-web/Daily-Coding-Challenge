a69b16985ee33684d151b3ac537d51a92cfe5b3b
dain <57316771+DainPark-web@users.noreply.github.com>
2022-01-31 13:14:55 +0100

    d

--- Challenges/javascript/lvl03/01Directions.js
+++ Challenges/javascript/lvl03/01Directions.js
@@ -3,58 +3,53 @@
 // And I need to improve this code. It looks so bad.
 function dirReduc(arr){
     const dir = arr;
-
-    
-    const northDir = dir.filter((item, index) => item === "NORTH");
-    const southDir = dir.filter((item, index) => item === "SOUTH");
-    const eastDir = dir.filter((item, index) => item === "EAST");
-    const westDir = dir.filter((item, index) => item === "WEST");
-
     let result = []
 
-    if(northDir.length === 1 && southDir.length ===1 && eastDir.length ===1 && westDir.length ===1){
-      result.push(northDir[0])
-      result.push(westDir[0])
-      result.push(southDir[0])
-      result.push(eastDir[0])
-    }else{
-      const ns = northDir.length - southDir.length;
-      const ew = eastDir.length - westDir.length;
-      if(ew !== 0){
-        if(ew > 0){
-          for(let i =0; i<ew; i++){
-            result.push("EAST")
-          }
-        }
-        if(ew < 0){
-          for(let i =0; i<Math.abs(ew); i++){
-            result.push("WEST");
-          }
+    dir.forEach((item, index) => {
+     
+      if(item === "NORTH"){
+        const found = dir.indexOf("SOUTH");
+        if(found !== -1){
+          dir.splice(found, 1, null);
+          dir.splice(index, 1, null);
         }
       }
-      if(ns !== 0){
-        if(ns > 0){
-          for(let i =0; i<ns; i++){
-            result.push("NORTH")
-          }
-        }
-        if(ns < 0){
-          for(let i =0; i<Math.abs(ns); i++){
-            result.push("SOUTH");
-          }
-        }
-      }
-      
-      result = result.reverse();
-    }
-    
 
+      if(item === "SOUTH"){
+        const found = dir.indexOf("NORTH");
+        if(found !== -1){
+          dir.splice(found, 1, null);
+          dir.splice(index, 1, null);
+        }
+      }
+      if(item === "EAST"){
+        const found = dir.indexOf("WEST");
+        if(found !== -1){
+          dir.splice(found, 1, null);
+          dir.splice(index, 1, null);
+        }
+      }
+      if(item === "WEST"){
+        const found = dir.indexOf("EAST");
+        if(found !== -1){
+          dir.splice(found, 1, null);
+          dir.splice(index, 1, null);
+        }
+      }
+
+
+      result = dir.filter((item) => {
+        if(item !== null){
+          return item;
+        }
+      })
+
+    })
     
-    return result;
-    
+   
   }
 
 
-dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]);
+dirReduc([ "EAST", "WEST", "EAST", "WEST", "EAST"]);
 // dirReduc(["NORTH", "WEST", "SOUTH", "EAST"]);
 // ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]), ["WEST"]
