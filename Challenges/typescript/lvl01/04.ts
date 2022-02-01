da7124198187bd83c874e8f929bcc831349836fc
dain <57316771+DainPark-web@users.noreply.github.com>
2022-02-01 08:31:59 +0100

    update

---
+++ Challenges/typescript/lvl01/04.ts
@@ -0,0 +1,9 @@
+export function solution(nums: number[]): number[] {
+  
+    const curArray = nums.sort((a, b) => a-b);
+    
+    
+    return curArray; 
+  }
+
+//   [1, 2, 10, 50, 5])=== [1,2,5,10,50]
