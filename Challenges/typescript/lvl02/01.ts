7627d9e9bf5212c90fef86fdb714bf525d502b12
dain <57316771+DainPark-web@users.noreply.github.com>
2022-02-03 15:23:10 +0100

    udpate

--- Challenges/typescript/lvl02/01.ts
+++ Challenges/typescript/lvl02/01.ts
@@ -23,9 +23,8 @@ function decipherThis(str: string): string {
     });
 
 
+    return newArray.join(" "); 
 
-
-    return newArray.join(" ");
   }
 
   console.log(decipherThis('72eva 97 103o 97t 116sih 97dn 115ee 104wo 121uo 100o'))
