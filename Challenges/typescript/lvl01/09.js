b093f2b4d93b9dab6b8584462087593bb1bef995
dain <57316771+DainPark-web@users.noreply.github.com>
2022-02-04 00:49:26 +0100

    update

--- Challenges/typescript/lvl01/09.js
+++
@@ -1,6 +0,0 @@
-function removeDuplicateWords(s) {
-    var changeToA = s.split(" ");
-    var newS = Array.from(new Set(changeToA));
-    return newS.join(" ");
-}
-removeDuplicateWords('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta');
