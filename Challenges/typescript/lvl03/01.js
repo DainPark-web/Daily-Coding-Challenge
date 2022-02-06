9fd319b21f41647e4ca8d2a0d63df7b9724f3a49
dain <57316771+DainPark-web@users.noreply.github.com>
2022-02-06 12:15:52 +0100

    update

--- Challenges/typescript/lvl03/01.js
+++ Challenges/typescript/lvl03/01.js
@@ -1,1 +1,33 @@
-// Product of consecutive Fib numbers
+var productFib = function (prod) {
+    var n = 2;
+    var found = true;
+    var fib = [];
+    fib[0] = 0;
+    fib[1] = 1;
+    var result = 0;
+    while (found) {
+        fib[n] = fib[n - 2] + fib[n - 1];
+        if (fib[n] > prod) {
+            found = false;
+        }
+        n++;
+    }
+    var resultA = [];
+    for (var i = 0; i < fib.length; i++) {
+        var num = fib[i] * fib[i + 1];
+        if (num === prod) {
+            resultA.push(fib[i]);
+            resultA.push(fib[i + 1]);
+            resultA.push(true);
+            break;
+        }
+        if (num > prod) {
+            resultA.push(fib[i]);
+            resultA.push(fib[i + 1]);
+            resultA.push(false);
+            break;
+        }
+    }
+    return resultA;
+};
+productFib(5895);
--- Challenges/typescript/lvl03/01.ts
+++ Challenges/typescript/lvl03/01.ts
@@ -1,2 +1,46 @@
 // Product of consecutive Fib numbers
+type IReturn = number | boolean;
 
+const productFib = (prod: number):IReturn[] => {
+   
+    let n:number = 2;
+    let found = true;
+
+    let fib:number[] = [];
+    fib[0] = 0;
+    fib[1] = 1;
+
+    let result:number = 0;
+    while(found){
+        fib[n] = fib[n - 2]+fib[n-1];
+        if(fib[n] > prod){
+            found= false;
+        }
+
+        n++
+    }
+
+    const resultA:IReturn[] = [];
+
+    for(let i = 0; i < fib.length; i++){
+        const num = fib[i] * fib[i + 1];
+    
+        if(num === prod){
+            resultA.push(fib[i]);
+            resultA.push(fib[i + 1]);
+            resultA.push(true);
+            break;
+        }
+        if(num > prod){
+            resultA.push(fib[i]);
+            resultA.push(fib[i+1]);
+            resultA.push(false);
+            break;
+         
+        }
+    }
+
+    return resultA;
+  }
+
+ productFib(5895);
