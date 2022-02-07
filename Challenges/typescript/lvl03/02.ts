23936302037f0e607dded2d8e3d2e54fbf2b718b
dain <57316771+DainPark-web@users.noreply.github.com>
2022-02-07 09:11:24 +0100

    error

--- Challenges/typescript/lvl03/02.ts
+++ Challenges/typescript/lvl03/02.ts
@@ -1,16 +1,26 @@
-// Is my friend cheating?
+// Is my friend cheating?🍎error
 
-function removeNb (n: number):string {
-    let a:number[] = [];
+// function removeNb (n: number) {
+//     let a:number[] = [];
 
-    for(let i = 0; i <= n; i++){
-        a.push(i);
-    }
-    const total = a.reduce((a,b) => a+b,0);
+//     for(let i = 0; i <= n; i++){
+//         a.push(i);
+//     }
+//     const total = a.reduce((a,b) => a+b,0);
+//     // console.log(a);
+//     // console.log(total);
+//     let result = [];
+//     for(let i = 0; i< a.length; i++){
+//         for(let j = 0; j< a.length; j++){
+//             if(i + j === total){
+//                 result.push(j)
+//                 result.push(i)
+//                 break;
+//             }
+//         }
+//     }
+//     console.log(result);
+//     return result;
+// }
 
-    
-
-    return "";
-}
-
-removeNb(26);
+// removeNb(5);
