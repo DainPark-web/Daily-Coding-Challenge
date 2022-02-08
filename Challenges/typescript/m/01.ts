df8080bac47b926fb85ce2aa70d0fddb7d13b914
dain <57316771+DainPark-web@users.noreply.github.com>
2022-02-08 21:06:18 +0100

    update

---
+++ Challenges/typescript/m/01.ts
@@ -0,0 +1,16 @@
+// Dain : 
+// Da gibt es ein nummer, das weiter erhöhert.
+// Wenn das Nummer > 10 return 1,,, > 20 return ,,, >30 return 3,,,,
+
+function dainAufgabe(e: number):number{
+    const newN = Math.ceil(e/10);
+
+   
+    console.log(newN);
+    
+    return 0;
+}
+
+dainAufgabe(11);
+
+console.log("dain");
