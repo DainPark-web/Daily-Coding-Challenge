31a5e7bf3a2a18a86cb5ddf3e276253e7f17ae95
dain <57316771+DainPark-web@users.noreply.github.com>
2022-02-09 14:25:02 +0100

    update

--- Challenges/typescript/m/03.js
+++ Challenges/typescript/m/03.js
@@ -1,1 +1,8 @@
-console.log("dain");
+// return 0~9 = 1
+// return 10 ~ 19 = 2
+// return 20 ~ 19 = 3
+// ...
+var dainCall = function (e) {
+    return 0;
+};
+dainCall(1);
--- Challenges/typescript/m/03.ts
+++ Challenges/typescript/m/03.ts
@@ -1,1 +1,11 @@
-console.log("dain")
+// return 0~9 = 1
+// return 10 ~ 19 = 2
+// return 20 ~ 19 = 3
+// ...
+
+const dainCall = (e:number):number => {
+
+    return 0;
+}
+
+dainCall(1);
