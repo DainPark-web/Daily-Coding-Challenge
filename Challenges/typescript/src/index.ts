03a03dcb89e40638181ce2205b843e363de442bb
dain <57316771+DainPark-web@users.noreply.github.com>
2022-02-19 10:39:58 +0100

    updaate

--- Challenges/typescript/src/index.ts
+++ Challenges/typescript/src/index.ts
@@ -1,11 +1,27 @@
 // Find the unique string
 
-function add(x: number): any {
-    
-   console.log("testd")
-    return ""
-  }
+function findUniq(arr: string[]): string {
+  const uniqueA = arr.map((e) => e.split("").sort().join(""));
+  // const uniqueA = arr.map((e) => e.split("")).map((e) => Array.from(new Set(e)).sort()).map((e) => e.join(""));
+  const uniqueFind = Array.from(new Set(uniqueA));
 
-add(4);
+  console.log( uniqueA);
+  // console.log( uniqueFind);
+
+  let result:any = [];
+  for(let i =0; i < uniqueFind.length; i++){
+    const f = uniqueA.filter((e) => {
+      return e === uniqueFind[i];
+    });
+    
+    if(f.length < 2){
+      result.push(...f);
+    }
+  }
+  console.log(result);
+  return result;
+}
+
+findUniq([ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ]);
 
 export {};
