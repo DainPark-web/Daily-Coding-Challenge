38eb42804f6a7cda1b443476681879cb53a98b74
dain <57316771+DainPark-web@users.noreply.github.com>
2022-02-21 07:27:19 +0100

    update

--- Challenges/typescript/src/index.ts
+++ Challenges/typescript/src/index.ts
@@ -1,1 +1,31 @@
 //Wwice linear
+
+export function dblLinear(n: number): number {
+
+    //make U
+    let u:number[] = [1];
+    for(let i = 0; i < n; i++){
+        const firstN = (u[i] * 2) + 1;
+        const lastN = (u[i] * 3) + 1;
+        u.push(firstN);
+        u.push(lastN);
+    }
+    
+    const result:number[] = Array.from(new Set(u.sort((a,b) => a - b)))
+    console.log(result);
+    //find durch Index U
+    return result[n];
+}
+
+console.log(dblLinear(100));
+
+
+
+// The number u(0) = 1 is the first one in u.
+// For each x in u, then y = 2 * x + 1 and z = 3 * x + 1 must be in u too.
+// There are no other numbers in u.
+// Ex: u = [1, 3, 4, 7, 9, 10, 13, 15, 19, 21, 22, 27, ...]
+
+// assert.strictEqual(dblLinear(10), 22);
+// assert.strictEqual(dblLinear(20), 57);
+// assert.strictEqual(dblLinear(30), 91);
