237793ae0806eb6d8033ec64fd2e3c0250e723b4
dain <57316771+DainPark-web@users.noreply.github.com>
2022-02-28 22:43:48 +0100

    update

--- Challenges/typescript/src/index.ts
+++ Challenges/typescript/src/index.ts
@@ -1,1 +1,37 @@
 // Ninety Nine Thousand Nine Hundred Ninety Nine
+
+function numberToEnglish(x: number): string {
+
+    //0 ~ 19
+    const num = [ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+                  "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+
+    //20 ~ 99
+    const numB = ["twenty", "thirty", "fourty", "sixty","fifty", "seventy", "eighty", "ninety"];
+
+    if(x <= 19){
+      
+        return num[x];
+    }
+    else if(x > 19 && x <= 99){
+        const ft = Number(x.toString().split("")[0]);
+        const fn = Number(x.toString().split("")[1]);
+        // console.log(ft, fn);
+        const result = `${numB[ft-2]} ${fn != 0 ? num[fn] : ""}`;
+        // console.log(result)
+        return result
+    }
+    else if(x > 99 && x < 1000){
+        const ft = Number(x.toString().split("")[0]);
+        //  여기서 부터 십의자리 수 찾기 
+        console.log(ft)
+        const result = `${num[ft]} hundred`
+        return result;
+    }
+    else{
+        return "d"
+    }
+  
+}
+
+console.log(numberToEnglish(900));
