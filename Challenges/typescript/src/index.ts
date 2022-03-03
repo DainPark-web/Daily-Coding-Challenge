82f7b58053efd6190d3bfdc76bf9940e4c99d8ad
dain <57316771+DainPark-web@users.noreply.github.com>
2022-03-03 06:38:44 +0100

    update

--- Challenges/typescript/src/index.ts
+++ Challenges/typescript/src/index.ts
@@ -1,1 +1,20 @@
 // Fibo akin
+
+class G964 {
+    
+    
+    public static lengthSupUK(n: number, k: number): number {
+        
+        console.log("dain")
+        return 0;
+    }
+    
+    public static comp(n: number): number {
+       
+        return 0;
+    }    
+  
+}
+
+;
+console.log(G964.lengthSupUK(3,4));
